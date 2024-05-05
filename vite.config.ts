ca1592e7bb0d9dab071b7e1d482a407cf215d909
naufalandya <135545118+naufalandya@users.noreply.github.com>
2024-05-05 22:47:15 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/export default defineConfig({
+
 export default defineConfig({
+  // Other Vite config options...
   plugins: [react()],
-})
+
+  esbuild: {
+    target: 'esnext',
+    platform: 'linux',
+  }
+  // Other Vite config options...
+});
